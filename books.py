18a97c47e088491dfa463a06273517a000efcacd
Aurélien Petit <david.jacques@celeste.fr>
2025-12-30 18:07:08 +0100

    feat: books

--- books.py
+++ books.py
@@ -1,34 +1,18 @@
 from fastapi import FastAPI
-import asyncio
 
 
 app = FastAPI()
 
-@app.get("/")
-async def first_api():
-    return {"message": "Hello Eric!"}
+books = [
+    {'title': 'Title One', 'author': 'Author One', 'category' : 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category' : 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category' : 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category' : 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category' : 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category' : 'math'},
+]
 
 
-
-async def fetch(id, sleep_time):
-    print(f"Coroutine {id} starting to fetch data.")
-    await asyncio.sleep(sleep_time)
-    print(f"done for {id}")
-    return {"id": id, "data": f"Sample data from coroutine {id}"}
-
-
-async def main():
-    task1 = asyncio.create_task(fetch(1, 2))
-    task2 = asyncio.create_task(fetch(2, 3))
-
-    result1 = await task1
-    result2 = await task2
-
-    task3 = asyncio.create_task(fetch(3, 1))
-
-    result3 = await task3
-
-    print(result1, result2, result3)
-
-
-asyncio.run(main())
+@app.get("/books")
+async def read_all_books():
+    return books
