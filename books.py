2453e5b21fab0abca06fde1d991c4ac76b9281da
Aurélien Petit <david.jacques@celeste.fr>
2025-12-31 18:04:33 +0100

    feat: post done

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 
 app = FastAPI()
@@ -14,5 +14,35 @@ books = [
 
 
 @app.get("/books")
-async def read_all_books():
+async def all_books():
     return books
+
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title : str):
+    for book in books:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category : str):
+    books_to_return = []
+    for book in books:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_category_by_query(book_author : str, category : str):
+    books_to_return = []
+    for book in books:
+        if book.get("category").casefold() == category.casefold() and book.get("author").casefold() == book_author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    books.append(new_book)
