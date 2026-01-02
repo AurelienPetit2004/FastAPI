588cba0d10226483bbd30a72ab0c1f648bf30a34
Aurélien Petit <david.jacques@celeste.fr>
2026-01-02 18:23:44 +0100

    feat: pydantic done

---
+++ books.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="id is not needed on POST", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra" : {
+            "example" : {
+                "title" : "A new book",
+                "author" : "codingwithroby",
+                "description" : "A new description of a book",
+                "rating" : 5
+            }
+        }
+    }
+
+
+books = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return books
+
+
+@app.post("/create_book")
+async def create_book(book_request : BookRequest):
+    print(book_request)
+    new_book = Book(**book_request.model_dump())
+    books.append(find_book_id(new_book))
+
+
+def find_book_id(book : Book):
+    book.id = books[-1].id + 1 if len(books) > 0 else 1
+    return book
