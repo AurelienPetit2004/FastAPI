1c65eab5efc7ef38817968f6486e62f9c58e4649
Aurélien Petit <david.jacques@celeste.fr>
2026-01-02 12:21:02 +0100

    feat: done

--- books.py
+++ books.py
@@ -34,6 +34,15 @@ async def read_category_by_query(category : str):
     return books_to_return
 
 
+@app.get("/books/by_author/")
+async def by_author(author : str):
+    books_to_return = []
+    for book in books:
+        if book.get("author").casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.get("/books/{book_author}/")
 async def read_category_by_query(book_author : str, category : str):
     books_to_return = []
@@ -46,3 +55,27 @@ async def read_category_by_query(book_author : str, category : str):
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     books.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(books)):
+        if books[i].get('TiTlE'.casefold()).casefold() == updated_book.get('TiTlE'.casefold()).casefold():
+            books[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title : str):
+    for i in range(len(books)):
+        if books[i].get('title').casefold() == book_title.casefold():
+            books.pop(i)
+            break
+
+
+@app.get("/books/by_author/{author}")
+async def by_author(author : str):
+    books_to_return = []
+    for book in books:
+        if book.get("author").casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
