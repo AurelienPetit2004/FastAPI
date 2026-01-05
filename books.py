be86835367bd875571de3e7e08572366483faf98
Aurélien Petit <david.jacques@celeste.fr>
2026-01-05 16:02:12 +0100

    feat: done

--- books.py
+++ books.py
@@ -1,7 +1,6 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from pydantic import BaseModel, Field
-
 
 app = FastAPI()
 
@@ -12,13 +11,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date : int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -27,6 +28,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date : int = Field(le=2050, ge=1950)
 
     model_config = {
         "json_schema_extra" : {
@@ -34,30 +36,79 @@ class BookRequest(BaseModel):
                 "title" : "A new book",
                 "author" : "codingwithroby",
                 "description" : "A new description of a book",
-                "rating" : 5
+                "rating" : 5,
+                "published_date" : 2040
             }
         }
     }
 
 
 books = [
-    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
-    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
-    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 1959),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 1974),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'An awesome book!', 5, 2011),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2019),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2026),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 1990)
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return books
 
 
-@app.post("/create_book")
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id : int = Path(ge=1)):
+    for book in books:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Item not Found")
+
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating : int = Query(ge=1, le=5)):
+    books_to_return = []
+    for book in books:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def read_books_by_publish_date(published_date : int = Query(le=2050, ge=1950)):
+    books_to_return = []
+    for book in books:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book : BookRequest):
+    book_changed = False
+    for i in range(len(books)):
+        if books[i].id == book.id:
+            books[i] = Book(**book.model_dump())
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id : int = Path(ge=1)):
+    book_changed = False
+    for i in range(len(books)):
+        if books[i].id == book_id:
+            books.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.post("/create_book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request : BookRequest):
-    print(book_request)
     new_book = Book(**book_request.model_dump())
     books.append(find_book_id(new_book))
 
